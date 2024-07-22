dca559362ef6261843f9f90599961f6dffc5afe9
virgílio conceição césar <virgilio_cesar.dev@outlook.com>
2024-07-22 06:40:41 -0300

    update render

--- renderer.js
+++ renderer.js
@@ -23,20 +23,9 @@ document
       document.getElementById("clip-duration").value
     );
 
-    // Validar se o vídeo e o diretório foram selecionados
-    if (
-      inputVideo === "Nenhum vídeo selecionado" ||
-      outputDir === "Nenhum diretório selecionado"
-    ) {
+    if (!inputVideo || !outputDir || isNaN(clipDuration)) {
       document.getElementById("status").innerText =
-        "Por favor, selecione um vídeo e um diretório antes de iniciar o processamento.";
-      return;
-    }
-
-    // Validar a duração do clipe
-    if (isNaN(clipDuration) || clipDuration <= 0) {
-      document.getElementById("status").innerText =
-        "Por favor, insira um valor válido para a duração do clipe.";
+        "Por favor, selecione um vídeo, um diretório e insira um valor válido para a duração do clipe.";
       return;
     }
 
@@ -56,19 +45,11 @@ document
         clipDuration
       );
       document.getElementById("status").innerText = "Processamento concluído!";
-
-      // Exibir a notificação
-      const notification = document.getElementById("notification");
-      notification.style.display = "block";
+      document.getElementById("notification").style.display = "block";
     } catch (error) {
       document.getElementById("status").innerText = `Erro: ${error.message}`;
     }
   });
-
-// Fechar a notificação
-document.getElementById("notification-close").addEventListener("click", () => {
-  document.getElementById("notification").style.display = "none";
-});
 
 document.getElementById("theme-toggle").addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
@@ -76,4 +57,8 @@ document.getElementById("theme-toggle").addEventListener("click", () => {
   document.getElementById("theme-toggle").innerText = isDarkMode
     ? "Modo Claro"
     : "Modo Escuro";
+});
+
+document.getElementById("notification-close").addEventListener("click", () => {
+  document.getElementById("notification").style.display = "none";
 });
